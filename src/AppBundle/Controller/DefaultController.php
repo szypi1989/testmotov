37659f07e5256ec6d1fbf3e22656bca3450d0e0d
szypi <szypula89@onet.pl>
2018-06-17 16:03:21 +0200

    Append list cars pagination

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,9 +15,14 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-         $em = $this->getDoctrine()->getEntityManager();
-    $entities = $em->getRepository('AppBundle:Cars')->findAll();
-                return array('entities' => $entities);
-        // replace this example code with whatever you need
+        $em = $this->getDoctrine()->getEntityManager();
+        $query = $em->createQuery("SELECT a FROM AppBundle:Cars a");  
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+        $query, /* query NOT result */
+        $request->query->getInt('page', 1)/*page number*/,
+        5/*limit per page*/,array('defaultSortFieldName' => 'a.model', 'defaultSortDirection' => 'asc')
+        );    
+        return array('pagination' => $pagination);
     }
 }
