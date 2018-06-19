e797cff9817f335d2c7903c7a72b9a686f3e0a2b
szypi <szypula89@onet.pl>
2018-06-19 14:41:36 +0200

    clear code index controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,14 +15,14 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $em = $this->getDoctrine()->getEntityManager();
-        $query = $em->createQuery("SELECT a FROM AppBundle:Cars a");  
-        $paginator  = $this->get('knp_paginator');
-        $pagination = $paginator->paginate(
-        $query, /* query NOT result */
-        $request->query->getInt('page', 1)/*page number*/,
-        5/*limit per page*/,array('defaultSortFieldName' => 'a.model', 'defaultSortDirection' => 'asc')
-        );    
-        return array('pagination' => $pagination);
+        //$em = $this->getDoctrine()->getEntityManager();
+       // $query = $em->createQuery("SELECT a FROM AppBundle:Cars a");  
+       // $paginator  = $this->get('knp_paginator');
+       // $pagination = $paginator->paginate(
+       // $query, /* query NOT result */
+      //  $request->query->getInt('page', 1)/*page number*/,
+       // 5/*limit per page*/,array('defaultSortFieldName' => 'a.model', 'defaultSortDirection' => 'asc')
+      //  );    
+       // return array('pagination' => $pagination);
     }
 }
